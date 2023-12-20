f3bb981f8e18a4fd3fe94f9590051dd9889d5af4
jpalma75 <palmasanchez@hotmail.com>
2023-12-20 16:29:28 -0600

    Cambio de esquema de tablas para el caso de departamento
    Se liga el encargado directamente al empleado y no al departamento

--- bd/scripts/Esquema_Inicial_Credencial.sql
+++ bd/scripts/Esquema_Inicial_Credencial.sql
@@ -1,3 +1,21 @@
+CREATE ROLE gu_credencial WITH
+  LOGIN
+  SUPERUSER
+  INHERIT
+  CREATEDB
+  CREATEROLE
+  NOREPLICATION;
+
+
+CREATE DATABASE db_credencial
+    WITH 
+    OWNER = gu_credencial
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Spain.1252'
+    LC_CTYPE = 'Spanish_Spain.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 -- encargados definition
 
 -- Drop table
@@ -25,8 +43,7 @@ CREATE TABLE encargados (
 -- DROP TABLE departamentos;
 
 CREATE TABLE departamentos (
-	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
-	id_encargado int4 NULL,
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,	
 	nombre varchar(100) NOT NULL COLLATE "es-MX-x-icu",
 	cp varchar(6) NOT NULL,
 	direccion varchar(200) NOT NULL COLLATE "es-MX-x-icu",
@@ -35,8 +52,7 @@ CREATE TABLE departamentos (
 	fecha_creacion timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
 	modificado_por varchar(50) NULL DEFAULT CURRENT_USER,
 	fecha_modificacion timestamp NULL DEFAULT LOCALTIMESTAMP,	
-	CONSTRAINT departamentos_pk PRIMARY KEY (id),
-	CONSTRAINT departamentos_encargado_fk FOREIGN KEY (id_encargado) REFERENCES encargados(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+	CONSTRAINT departamentos_pk PRIMARY KEY (id)	
 );
 
 
@@ -49,6 +65,7 @@ CREATE TABLE departamentos (
 CREATE TABLE empleados (
 	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	id_departamento int4 NOT NULL,
+	id_encargado int4 NOT NULL,
 	id_empleado_anterior int4 NULL,
 	nombre varchar(50) NOT NULL COLLATE "es-MX-x-icu",
 	ap_paterno varchar(50) NOT NULL COLLATE "es-MX-x-icu",
@@ -70,5 +87,21 @@ CREATE TABLE empleados (
 	fecha_modificacion timestamp NULL DEFAULT LOCALTIMESTAMP,
 	CONSTRAINT empleado_pk PRIMARY KEY (id),
 	CONSTRAINT empleados_departamento_fk FOREIGN KEY (id_departamento) REFERENCES departamentos(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-	CONSTRAINT empleados_empleados_fk FOREIGN KEY (id_empleado_anterior) REFERENCES empleados(id)
+	CONSTRAINT empleados_empleados_fk FOREIGN KEY (id_empleado_anterior) REFERENCES empleados(id),
+	CONSTRAINT empleados_encargados_fk FOREIGN KEY (id_encargado) REFERENCES encargados(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
+
+
+INSERT INTO encargados
+(nombre, cargo, ruta_firma, estatus_registro, creado_por, fecha_creacion, modificado_por, fecha_modificacion)
+VALUES('Lic. Carlos Enrique Iñiguez Rosique', 'Secretario de Administración e Innovación Gubernamental', 'archivos/firmas/encargados/20231220120245.png', 'VIG', 'gu_credencial', '2023-12-19 14:02:17.203', 'gu_credencial', '2023-12-19 14:02:17.203');
+
+
+/*
+INSERT INTO departamentos
+(nombre, cp, direccion, estatus_registro, creado_por, fecha_creacion, modificado_por, fecha_modificacion, id_encargado)
+VALUES('Secretaría Particular', '86000', 'Palacio de Gobierno, Calle Independencia No.2 Col. Centro', 'VIG', 'gu_credencial', '2023-12-13 12:56:35.579', 'gu_credencial', '2023-12-13 12:56:35.579', NULL);
+INSERT INTO departamentos
+(nombre, cp, direccion, estatus_registro, creado_por, fecha_creacion, modificado_por, fecha_modificacion, id_encargado)
+VALUES('Secretaría Técnica', '86000', 'Palacio de Gobierno, Calle Independencia No.2 Col. Centro', 'VIG', 'gu_credencial', '2023-12-13 12:57:05.899', 'gu_credencial', '2023-12-13 12:57:05.899', NULL);
+*/
