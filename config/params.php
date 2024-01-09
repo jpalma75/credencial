e37efe7f4bd51f8a9e5448c7d6d300ee527f1e9c
jpalma75 <palmasanchez@hotmail.com>
2024-01-08 19:39:12 -0600

    Agregado de parametro tipo de contrato
    Adicionado del parámetro tipo de contrato "TipoContrato"

--- config/params.php
+++ config/params.php
@@ -12,6 +12,7 @@ return [
     'SistemaNombre'      => 'Sistema de Impresión de Credenciales',
     'SistemaSiglas'      => 'CREDENCIAL',
     'SistemaDescripcion' => 'Sistema que permite dar de alta responsables, departamentos y empleados de los cuales se generará credencial',
+    'TipoContrato'       => 'CONFIANZA',
 
     //Rutas para los archivos
     'FirmasEncargados'   => 'archivos/firmas/encargados/',
